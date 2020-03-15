859af33fd3284bac5da5afd5e70e802a912387ec
Amadeusz Masny <amcia94@gmail.com>
2020-03-15 15:02:46 +0100

    - pytchon-chess env initial structure

---
+++ chess_ai_gym/environments/pytchon_chess/board.py
@@ -0,0 +1,6 @@
+from chess import Board as BaseBoard
+
+
+class Board(BaseBoard):
+    def __init__(self):
+        super().__init__()
---
+++ chess_ai_gym/environments/pytchon_chess/game.py
@@ -0,0 +1,6 @@
+from chess.pgn import Game as BaseGame
+
+
+class Game(BaseGame):
+    def __init__(self):
+        super().__init__()

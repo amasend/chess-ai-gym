6b1cade7d7cfdc2c2fa671197e5bb4752016095a
Amadeusz Masny <amcia94@gmail.com>
2020-03-15 13:03:49 +0100

    - initial repo structure

---
+++ setup.py
@@ -0,0 +1,35 @@
+import setuptools
+
+with open('VERSION.txt', 'r') as f:
+    version = f.read()
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/amasend/chess-ai-gym/issues',
+    'Source Code': 'https://github.com/amasend/chess-ai-gym'
+}
+
+setuptools.setup(
+    name="chess_ai_gym",
+    version=version,
+    author="Amaedeusz Masny",
+    author_email="amadeuszmasny@gmail.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    install_requires=[
+        'async-lichess-sdk',
+    ],
+    url="https://github.com/amasend/chess-ai-gym",
+    packages=setuptools.find_packages(exclude=["tests"]),
+    license="Apache Software License",
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
